868a930ab49ce1405608f3671c364156916f5060
Per Stian Hoff <per.hoff@soprasteria.com>
2025-11-10 13:56:16 +0100

    Refactor RabbitMQ connection handling and update README with user secrets instructions

--- PublishMessage/PublishMessageFunction.cs
+++ PublishMessage/PublishMessageFunction.cs
@@ -1,24 +1,29 @@
-using EventDrivenCommon;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
+using System.Threading.Channels;
 
 namespace PublishMessage
 {
     public class PublishMessageFunction
     {
         private readonly ILogger<PublishMessageFunction> _logger;
+        private readonly IConfiguration _configuration;
 
-        public PublishMessageFunction(ILogger<PublishMessageFunction> logger)
+        public PublishMessageFunction(ILogger<PublishMessageFunction> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         [Function("PublishMessageFunction")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req, string message="Default Value Code Academy")
         {
             
 
